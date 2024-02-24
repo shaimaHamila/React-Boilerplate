77846d8f2079d585607e6474a7a7d272c27a8500
shaimaHamila <hamilachaima18@gmail.com>
2024-02-24 20:35:44 +0100

    setup redux toolkit and update styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
-
+import { useSelector } from "react-redux";
+import { fetchUsers, selectUsers } from "./features/user/usersSlice";
+import { store } from "./store/store";
+import { useEffect } from "react";
 function App() {
-  const [count, setCount] = useState(0);
+  const users = useSelector(selectUsers);
+  useEffect(() => {
+    store.dispatch(fetchUsers());
+  }, []);
   console.log(import.meta.env.VITE_ENVIRONMENT);
   return (
     <>
       <h1>Vite + React</h1>
-      <div className='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR lets try 123 hi
-        </p>
+      <div>
+        <div>
+          <h2> List of users </h2>
+          {users.loading && <div>Loding ...</div>}
+          {!users.loading && users.error ? <div>{}</div> : null}
+          {!users.loading && users.users.length > 0 ? (
+            <ul>
+              {users.users.map((user, key) => (
+                <li key={key}>{user.name}</li>
+              ))}
+            </ul>
+          ) : null}
+        </div>
       </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more OH nice really</p>
+      <p className='read-the-docs'></p>
 
       <Routes>
-        <Route path='/signin' element={<Login />}></Route>
+        <Route path='/' element={<Login />}></Route>
         <Route path='/signup' element={<Signup />}></Route>
       </Routes>
     </>
